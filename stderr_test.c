94e837195dfc7fdbbf910118c3c4d0c1e68163b6
Stefan Liao <stefan.liao.cs11@nycu.edu.tw>
2024-03-01 22:21:17 +0800

    [feat] Can now fetch address to another program  * Also add log file record for furthre research

---
+++ stderr_test.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024 // Increased buffer size
+
+int main() {
+  // Create pipe descriptors
+  int pipe_fd[2];
+  if (pipe(pipe_fd) == -1) {
+    perror("pipe");
+    exit(EXIT_FAILURE);
+  }
+
+  // Fork a child process
+  pid_t pid = fork();
+  if (pid == -1) {
+    perror("fork");
+    exit(EXIT_FAILURE);
+  }
+
+  if (pid == 0) { // Child process (Program A)
+    // Close the read end of the pipe
+    close(pipe_fd[0]);
+
+    // Redirect stdout to the pipe
+    dup2(pipe_fd[1], STDOUT_FILENO);
+
+    // Close unused pipe descriptors
+    close(pipe_fd[1]);
+
+    // Change the working directory
+    if (chdir("/home/dreyex/Documents/Research/PPT") == 0) {
+      printf("Changed directory to /path/to/new_directory\n");
+    } else {
+      perror("chdir() error");
+      return 1;
+    }
+
+    // Execute Program A
+    execl("/home/dreyex/Documents/Research/PPT/./vg-in-place", "vg-in-place",
+          "--tool=cachegrind", "--instr-at-start=no", "--cache-sim=yes",
+          "--D1=49152,12,64", "--I1=32768,8,64", "--L2=1310720,10,64", "-v",
+          "-d", "--log-fd=1", "/home/dreyex/use_this/jacobi-2d", NULL);
+    perror("execl");
+    exit(EXIT_FAILURE);
+  } else { // Parent process (Program B)
+    // Close the write end of the pipe
+    int counter = 0;
+    close(pipe_fd[1]);
+
+    // Read from the pipe continuously
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_read;
+    char cache[BUFFER_SIZE * 2]; // Double the buffer size for caching
+    size_t cache_index = 0;
+    int stop_receiving = 0;
+    while (!stop_receiving &&
+           (bytes_read = read(pipe_fd[0], buffer, BUFFER_SIZE)) > 0) {
+      // Check if caching the data would exceed the buffer size
+      if (cache_index + bytes_read >= sizeof(cache)) {
+        fprintf(stderr, "Error: Buffer overflow\n");
+        exit(EXIT_FAILURE);
+      }
+
+      // Cache the received data
+      memcpy(cache + cache_index, buffer, bytes_read);
+      cache_index += bytes_read;
+
+      // Check if there is a complete line in the cache
+      char *line_start = cache;
+      char *line_end;
+      while ((line_end = strchr(line_start, '\n')) != NULL) {
+        // Null-terminate the line
+        *line_end = '\0';
+
+        // Check if the line starts with '%'
+        if (line_start[0] == '*') {
+          // Output the line
+          printf("detected at %s\n", line_start);
+          counter++;
+        }
+
+        // Check if the line starts with '#', then stop receiving
+        if (line_start[0] == '#') {
+          stop_receiving = 1;
+          break;
+        }
+
+        // Move to the next line
+        line_start = line_end + 1;
+      }
+
+      // Move the remaining data to the beginning of the cache
+      size_t remaining_bytes = cache_index - (line_start - cache);
+      memmove(cache, line_start, remaining_bytes);
+      cache_index = remaining_bytes;
+    }
+    printf("There are %d line of output to here\n", counter);
+
+    // Close the read end of the pipe
+    close(pipe_fd[0]);
+
+    // Wait for the child process to finish
+    int status;
+    waitpid(pid, &status, 0);
+  }
+
+  return 0;
+}
