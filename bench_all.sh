eaf295cbdd666a83b525ca7722ca8e9a7ba26b61
Stefan Liao <stefan.liao.cs11@nycu.edu.tw>
2024-04-18 10:34:08 +0800

    [feat] Add generation of execution comparison graph * Using gnuplot

---
+++ bench_all.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+# Don't forget to modify the dataset size you want to examine
+# could choose MINI_DATASET, SMALL_DATASET, MEDIUM_DATASET, LARGE_DATASET, EXTRALARGE_DATASET
+data_size=SMALL_DATASET
+# src is the main directory of the benchmark
+src=/home/dreyex/Documents/Research/TraceBench
+# dst is the directory where the traces will be saved
+dst=/home/dreyex/use_this
+ppcg_bin=`pwd`/ppcg/install/bin
+now=""
+
+# Function to get the current time in milliseconds
+get_current_time() {
+    date +%s%N
+}
+
+# Function to calculate the elapsed time in seconds with milliseconds precision
+calculate_elapsed_time() {
+    local start_time=$1
+    local end_time=$2
+    local elapsed_seconds=$(( (end_time - start_time) / 1000000000 ))
+    local elapsed_milliseconds=$(( (end_time - start_time) % 1000000000 / 1000000 ))
+    echo "$elapsed_seconds.$elapsed_milliseconds"
+}
+
+# Time to generate the trace for simulation
+run_test_a() {
+    local item_basename=$1
+    ./vg-in-place --tool=cachegrind --instr-at-start=no --cache-sim=yes --D1=49152,12,64 --I1=32768,8,64 --L2=1310720,10,64 -v --cachegrind-out-file="cachegrind.out.$item_basename" --log-fd=1  $dst/obj/$item_basename | grep -v "-" | grep -v "=" >  $dst/trace/$item_basename.log
+    mv cachegrind.out.$now $dst/trace
+}
+
+# Simulation the trace in the CoreSim LRU
+run_test_b() {
+    local item_basename=$1
+    ./CoreSim $dst/trace/$item_basename.log
+}
+
+# Use polyhedral trace to simulate the trace instead of test_a and test_b
+run_test_c() {
+    local item_basename=$1
+    ./CorePPT $dst/obj/$item_basename $ppcg_bin -D$data_size
+}
+
+# Initialize arrays to store test timings
+
+echo "Item Generate_trace Simulate_trace Polyhdral_Simulate" > timings.dat
+
+# Iterate through each item in the benchmark list
+while IFS= read -r i; do
+    echo "" # Line change for better readability
+    echo "Processing $i"
+    now=$(basename $i .c)
+    name_of_test+=$now
+    gcc -Og -g -o $dst/obj/$now $src/$i $src/utilities/polybench.c -I $src/utilities -D$data_size -DDATA_TYPE_IS_FLOAT -DPOLYBENCH_USE_SCALAR_LB -DCACHEGRIND
+    echo "Compile Succeeded"
+
+    # Execute each test function and collect timings
+    start_time=$(get_current_time)
+    test_result=("$(run_test_a $now)")
+    finish_time=$(get_current_time)
+    elapsed_time=$(calculate_elapsed_time "$start_time" "$finish_time")
+    echo "Finish generating trace for $now with $elapsed_time seconds."
+    test_a_timings=$elapsed_time
+
+    start_time=$(get_current_time)
+    test_result=("$(run_test_b $now)")
+    finish_time=$(get_current_time)
+    elapsed_time=$(calculate_elapsed_time "$start_time" "$finish_time")
+    echo "Finish generating trace for $now with $elapsed_time seconds."
+    test_b_timings=$elapsed_time
+
+
+    start_time=$(get_current_time)
+    test_result=("$(run_test_c $now)")
+    finish_time=$(get_current_time)
+    elapsed_time=$(calculate_elapsed_time "$start_time" "$finish_time")
+    echo "Finish generating trace for $now with $elapsed_time seconds."
+    test_c_timings=$elapsed_time
+
+    printf "%s %s %s %s\n" "$now" "$test_a_timings" "$test_b_timings" "$test_c_timings" >> timings.dat
+
+done < benchmark_list
+
+du -sH $dst/trace/* | sort -n
+rm -rf *.cu *.hu
+
+# Generate bar plot using gnuplot
+gnuplot <<- EOF
+    set terminal pngcairo enhanced font "arial,10" size 800,600
+    set output 'benchmark_plot.png'
+    set title "Benchmark Test Timings"
+    set ylabel "Time (seconds)"
+    set xlabel "Items"
+    set xtics rotate by -45
+    set style data histogram
+    set style histogram cluster gap 1
+    set style fill solid 1.0 border -1
+    plot 'timings.dat' using 2:xtic(1) title "Test A", '' using 3 title "Test B", '' using 4 title "Test C"
+EOF
+
+echo "Plot generated: benchmark_plot.png"
---
+++ bench_exe_generate.sh
@@ -0,0 +1,36 @@
+# This is a file to generate the original size of the program trace
+# And the reduced size of the program trace is not yet implemented
+
+# src is the main directory of the benchmark
+src=/home/dreyex/Documents/Research/TraceBench
+# dst is the directory where the traces will be saved
+dst=/home/dreyex/use_this
+# could choose MINI_DATASET, SMALL_DATASET, MEDIUM_DATASET, LARGE_DATASET, EXTRALARGE_DATASET
+data_size=MEDIUM_DATASET
+
+list=./benchmark_list
+now=""
+
+for i in `cat $list`
+do
+    echo "Processing $i"
+    now=$(basename $i .c)
+    # Line debug
+    # echo "Compiling $now from $src/$i $src/utilities/polybench.c to $dst/obj/$now"
+    gcc -Og -g -o $dst/obj/$now $src/$i $src/utilities/polybench.c -I $src/utilities -D$data_size -DDATA_TYPE_IS_FLOAT -DPOLYBENCH_USE_SCALAR_LB -DCACHEGRIND
+    echo "Compile Succeeded"
+    # Version of valgrind don't forget to define the DEBUG macro
+done
+
+for i in `cat $list`
+do
+    now=$(basename $i .c)
+    echo "Running $now"
+    start_time=$(date +%s)
+    ./vg-in-place --tool=cachegrind --instr-at-start=no --cache-sim=yes --D1=49152,12,64 --I1=32768,8,64 --L2=1310720,10,64 -v --cachegrind-out-file="cachegrind.out.$now" --log-fd=1  $dst/obj/$now | grep -v "-" | grep -v "=" >  $dst/trace/$now.log
+    finish_time=$(date +%s)
+    echo "Time duration: $((finish_time - start_time)) secs."
+    mv cachegrind.out.$now $dst/trace
+done
+
+du -sH $dst/trace/* | sort -n
